57596343e32fa77ab962f5d3250a1195cb2ee92a
Danilo Silva <102080952+Danilo-Ccomp@users.noreply.github.com>
2025-04-01 19:05:54 -0300

    Update ProcessoSeletivo.java

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,38 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo{
     public static void main(String[] args) {
-      imprimriSelecionados();
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "MONICA"};
+      for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+      }
     }
-      
+    
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando= true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else 
+                System.out.println("CONTATO REALIZADO COM SUCESSO!");
+                
+        }while(continuarTentando && tentativasRealizadas < 3);
+        
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA REALIZADA");
+        else    
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM O " + candidato + ", NUMERO MAXIMO DE TENTATIVA EXCEDIDO!");
+    }
+
+    static boolean  atender(){
+        return new Random().nextInt(3) == 1;
+    }
+
     static void imprimriSelecionados(){
         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "MONICA"};
         for(int indice = 0; indice < candidatos.length; indice++){
